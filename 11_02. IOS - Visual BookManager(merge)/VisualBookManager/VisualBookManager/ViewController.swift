89552dfcc877c07f10e87c44b3a70fb9152caa3c
KwonJH <hojak99@gmail.com>
2018-03-15 14:11:32 +0900

    11_02. IOS - Visual BookManager(merge) : 책 삭제 기능 추가

--- 11_02. IOS - Visual BookManager(merge)/VisualBookManager/VisualBookManager/ViewController.swift
+++ 11_02. IOS - Visual BookManager(merge)/VisualBookManager/VisualBookManager/ViewController.swift
@@ -68,6 +68,14 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func removeAction(_ sender : Any) {
+        let title = titleTextField.text!;
+        let result = library.removeBook(title: title);
+        
+        countLabel.text = "\(library.countBook())";
+        outputTextView.text = result;
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
