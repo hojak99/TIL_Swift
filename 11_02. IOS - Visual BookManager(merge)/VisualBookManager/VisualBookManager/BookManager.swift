988cf38f561de8e7050b5c3f4d4e9e276967b167
KwonJH <hojak99@gmail.com>
2018-03-15 11:34:18 +0900

    11_02. IOS - Visual BookManager(merge) 버튼 클릭 이벤트 추가

---
+++ 11_02. IOS - Visual BookManager(merge)/VisualBookManager/VisualBookManager/BookManager.swift
@@ -0,0 +1,74 @@
+//
+//  BookManager.swift
+//  BookManager
+//
+//  Created by iquest on 3/14/18.
+//  Copyright © 2018 kjh. All rights reserved.
+//
+import Foundation
+
+class BookManager {
+    
+    // Book struct를 관리할 배열
+    var bookList = [Book]();
+    
+    /*
+     책 색성
+     */
+    func registerBook(newBook: Book) {
+        bookList += [newBook];
+    }
+    
+    /*
+     모든 책 정보를 반환
+     */
+    func showAllBooks() -> String {
+        // return bookList.description;  다음의 코드는 array  데이터를 출력한다.  Java의 toString() 메소드와 비슷함.
+        var strTemp = "";
+        for tempBook in bookList {
+            strTemp += "Title : \(tempBook.title)\n";
+            strTemp += "Field : \(tempBook.field)\n";
+            strTemp += "Author : \(tempBook.author)\n";
+            strTemp += "-----------------\n";
+        }
+        return strTemp;
+    }
+    
+    /*
+     등록된 책들의 개수를 반환
+     */
+    func countBook() -> Int{
+        return bookList.count;
+    }
+    
+    /*
+     등록된 책 정보가 없으면 nil 반환
+     */
+    func searchBook(bookName: String) -> String? {
+        // = "" 와 같은 의미
+        var strTemp = String();
+        for tempBook in bookList {
+            if tempBook.title == bookName {
+                strTemp += "Title : \(tempBook.title)\n";
+                strTemp += "Field : \(tempBook.field)\n";
+                strTemp += "Author : \(tempBook.author)\n";
+                strTemp += "-----------------\n";
+                return strTemp;
+            }
+        }
+        return nil;
+    }
+    
+    /*
+     등록된 책을 삭제
+     */
+    func removeBook(title: String) -> String {
+        for (index, value) in bookList.enumerated() {
+            if value.title == title {
+                bookList.remove(at: index);
+                return "[\(title)] 이란 책을 삭제했습니다."
+            }
+        }
+        return "[\(title) 이란 책을 삭제하지 못 했습니다.]"
+    }
+}
---
+++ 11_02. IOS - Visual BookManager(merge)/VisualBookManager/VisualBookManager/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  VisualBookManager
+//
+//  Created by iquest on 3/14/18.
+//  Copyright © 2018 kjh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var library = BookManager();
+
+    
+    /*
+        초기화 코드 작성하는 메소드
+     */
+    override func viewDidLoad() {
+    
+        let book1 = Book(title: "신과함께", field: "웹툰", author: "주호민");
+        let book2 = Book(title: "모든 순간이 너였다 ", field: "시/에세이", author: "하태완");
+        let book3 = Book(title: "나미야 잡화점의 기적", field: "추리소설", author: "히가시노 게이고");
+
+        library.registerBook(newBook: book1);
+        library.registerBook(newBook: book2);
+        library.registerBook(newBook: book3);
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
