e40e54b7f24d9f4f0dacc3935a327c2af9824798
KwonJH <hojak99@gmail.com>
2018-03-16 09:41:05 +0900

    12_01. IOS - DigitalFrame(merge) : 애니메이션 플레이 속도 변경할 수 있는 slider 기능 추가

--- 12_01. IOS - DigitalFrame(merge)/DigitalFrame/DigitalFrame/ViewController.swift
+++ 12_01. IOS - DigitalFrame(merge)/DigitalFrame/DigitalFrame/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imgView: UIImageView!;
+    @IBOutlet weak var toggleButton : UIButton!;
+    @IBOutlet weak var speedSlider : UISlider!;
+    @IBOutlet weak var speedLabel : UILabel!;
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +27,28 @@ class ViewController: UIViewController {
         
         imgView.animationImages = arrImage;
         imgView.animationDuration = 7;
+        speedLabel.text = String(format: "%.2f", speedSlider.value);
         
     }
 
+    @IBAction func speedSliderAction(_ sender : Any) {
+        imgView.animationDuration = Double(speedSlider.value);
+        
+        // 값 변경되면 멈추기 때문.
+        toggleButton.setTitle("STOP", for: UIControlState.normal);
+        imgView.startAnimating();
+        
+        // 소수점 2째자리
+        speedLabel.text = String(format: "%.2f", speedSlider.value);
+    }
+    
     @IBAction func toggleButton(_ sender : Any) {
         if imgView.isAnimating == true {
             imgView.stopAnimating();
+            toggleButton.setTitle("START", for: UIControlState.normal);
         } else {
             imgView.startAnimating();
+            toggleButton.setTitle("STOP", for: UIControlState.normal);
         }
     }
     
