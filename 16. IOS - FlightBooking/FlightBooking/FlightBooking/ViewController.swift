508d49fcc956b4844932a7c37b7ea761caf26f41
KwonJH <hojak99@gmail.com>
2018-03-21 17:51:49 +0900

    16. IOS - FlightBooking : 버튼 클릭 시 hidden처리 되도록

--- 16. IOS - FlightBooking/FlightBooking/FlightBooking/ViewController.swift
+++ 16. IOS - FlightBooking/FlightBooking/FlightBooking/ViewController.swift
@@ -17,7 +17,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        returnDateLabel.isHidden = true;
+        returnDateButton.isHidden = true;
+        selectDatePicker.isHidden = true;
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,12 +29,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showDatePickerAction(_ sender: Any) {
+       
+        if selectDatePicker.isHidden == false {
+            selectDatePicker.isHidden = true;
+        } else {
+            selectDatePicker.isHidden = false;
+        }
     }
    
-    @IBAction func showReturnDateAction(_ sender: Any) {
+    // (_ sender: UISwitch) 로도 가능
+    @IBAction func showReturnDateAction(_ sender: UISwitch) {
+        
+        returnDateButton.isHidden = !sender.isOn;
+        returnDateLabel.isHidden = !sender.isOn;
+        
+//        형변환하는 코드
+//        if (sender as! UISwitch).isOn {
+//            returnDateButton.isHidden = false;
+//            returnDateLabel.isHidden = false
+//        } else {
+//            returnDateLabel.isHidden = true;
+//            returnDateButton.isHidden = true;
+//        }
+        
+//        if returnDateButton.isHidden == true {
+//            returnDateButton.isHidden = false;
+//            returnDateLabel.isHidden = false
+//        } else {
+//            returnDateLabel.isHidden = true;
+//            returnDateButton.isHidden = true;
+//        }
     }
     
     @IBAction func selectedDateAction(_ sender: Any) {
+        departureDateButton.setTitle("hello", for: UIControlState.normal)
     }
     
     
