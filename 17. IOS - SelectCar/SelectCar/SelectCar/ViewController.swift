90c8572145e891392235defd4c784d3f7fe6475d
KwonJH <hojak99@gmail.com>
2018-03-22 10:49:10 +0900

    17. IOS 0 SelectCar : Design 완료

---
+++ 17. IOS - SelectCar/SelectCar/SelectCar/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SelectCar
+//
+//  Created by iquest on 3/22/18.
+//  Copyright © 2018 kjh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!;
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
