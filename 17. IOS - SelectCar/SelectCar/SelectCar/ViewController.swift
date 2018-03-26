8fb0bbecb52cf1be13f9d7ce430a8506b4239511
KwonJH <hojak99@gmail.com>
2018-03-26 14:07:52 +0900

    17. IOS - SelectCar : pickerView 각 항목 배치 완료

--- 17. IOS - SelectCar/SelectCar/SelectCar/ViewController.swift
+++ 17. IOS - SelectCar/SelectCar/SelectCar/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     override func viewDidLoad() {
         super.viewDidLoad();
         
-        model = animal;
+        model = human;
         imageView.layer.cornerRadius = 50.0;
         imageView.layer.masksToBounds = true;
     }
@@ -35,14 +35,21 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         // Dispose of any resources that can be recreated.
     }
     
+    /*
+        컴포넌트 몇개 만드는지 return 하면 된다.
+        여기서 컴포넌트는 칸? 을 생각하면 된다.
+        만약 3을 리턴하면 pickerView 는 ["human", "animal"], ["dog", "panda", "cat", "etc", "man", "girl"], [~] 로 생성이 될 것이다.
+     */
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2;
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         if component == 0 {
+            // 첫 번째 component 에는 groupName 배열의 값이 들어감
             return groupName[row];
         } else {
+            // 두 번째 component 부터는 animal 배열의 값과, human 배열의 값이 들어간다.
             return model[row];
         }
     }
@@ -53,6 +60,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         } else {
             return model.count;
         }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if component == 0 && row == 0 {
+            model = human;
+        } else if component == 0 && row == 1 {
+            model = animal;
+        }
+        
+        pickerView.reloadAllComponents();
     }
 }
 
