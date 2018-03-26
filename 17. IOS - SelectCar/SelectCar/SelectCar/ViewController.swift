40214320c8e63b20fcedef056c84e7765cf6682e
KwonJH <hojak99@gmail.com>
2018-03-26 13:49:33 +0900

    17. IOS - SelectCar : PicketView delegate 설정 및 셋팅 완료

--- 17. IOS - SelectCar/SelectCar/SelectCar/ViewController.swift
+++ 17. IOS - SelectCar/SelectCar/SelectCar/ViewController.swift
@@ -8,15 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var imageView: UIImageView!;
     
+    let groupName = ["human", "animal"];
+    var model = [String]();
+    var modelImage = [String]();        // 이미지뷰에 로딩할 이미지들
     
+    let animal = ["dog", "panda", "cat", "etc"];
+    let human = ["man", "girl"];
+    
+    let animalImageNames = ["02.jpg","01.jpg", "04.jpg", "03.jpg"];
+    let humanImageNames = ["06.jpg"];
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        imageView.layer.cornerRadius = 20.0;
+        super.viewDidLoad();
+        
+        model = animal;
+        imageView.layer.cornerRadius = 50.0;
         imageView.layer.masksToBounds = true;
     }
 
@@ -24,7 +34,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component == 0 {
+            return groupName[row];
+        } else {
+            return model[row];
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if component == 0 {
+            return groupName.count;
+        } else {
+            return model.count;
+        }
+    }
 }
 
