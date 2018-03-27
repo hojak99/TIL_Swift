8198796277be373e6e9d1ff54b26370110335eb4
KwonJH <hojak99@gmail.com>
2018-03-27 10:34:25 +0900

    18. IOS - TabbarEX : Tabbar 기능 추가 완료

---
+++ 18. IOS - TabbarEX/TabbarEX/TabbarEX/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  TabbarEX
+//
+//  Created by iquest on 3/27/18.
+//  Copyright © 2018 kjh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        outputLabel.text = "2번 버튼 클릭"
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
