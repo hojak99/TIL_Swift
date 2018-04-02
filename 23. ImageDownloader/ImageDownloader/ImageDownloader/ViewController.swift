08cdd5c759205c5f6a43fc0fbe78139e395be0f9
KwonJH <hojak99@gmail.com>
2018-04-02 17:28:58 +0900

    23. ImageDownloader : urlsession 을 이용해 다운로드 셋팅 완료

--- 23. ImageDownloader/ImageDownloader/ImageDownloader/ViewController.swift
+++ 23. ImageDownloader/ImageDownloader/ImageDownloader/ViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
 
+    
+    @IBOutlet weak var imgView: UIActivityIndicatorView!
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var downloadTask: URLSessionDownloadTask!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,31 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        
+    }
 
+    @IBAction func downloadAction(_ sender: Any) {
+        let sessionConfiguration = URLSessionConfiguration.default;
+        let session = URLSession(configuration: sessionConfiguration, delegate: self, delegateQueue: OperationQueue.main);
+        
+        downloadTask = session.downloadTask(with: URL(string: "https://raw.githubusercontent.com/ChoiJinYoung/iphonewithswift2/master/sample.jpeg")!);
+        
+        // 해담 메소드를 호출해야 작업을 시작한다.
+        downloadTask.resume();
+    }
 
+    @IBAction func suspendAction(_ sender: Any) {
+    }
+    @IBAction func resumeAction(_ sender: Any) {
+    }
+    
+    @IBAction func cancelAction(_ sender: Any) {
+    }
 }
 
